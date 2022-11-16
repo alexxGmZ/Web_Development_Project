570f70eda659758018b1987939ccd56bbcbdbbbd
earlylalo <91583055+earlylalo@users.noreply.github.com>
2022-11-15 19:15:39 -0800

    added profile pic, name, and bio containers

--- user_profile.php
+++ user_profile.php
@@ -33,15 +33,45 @@
 		// var_dump($_SESSION['user_info']);
 		// echo '</pre>';
 	?>
-	<div class="text-center">
+	<!-- <div class="text-center">
 		<h1 class="text-dark fw-bold p-3">Your Profile</h1>
-	</div>
+	</div> -->
+	
 	<div class="profile_area border shadow-lg rounded-3 p-3">
+		<center>
+			<!-- profile picture -->
+			<div class="picture-container">
+				<img class="profile-picture" src="https://seedpsychology.com.au/wp-content/uploads/2018/09/Damian-profile-pic-square.jpg" alt="User_image">
+			</div>
+
+			<!-- name and user-name -->
+			<div class="name-container">
+				<p class="name">
+					Profile Name
+					<span class="username">
+						(User Name)
+					</span>
+				</p>
+			</div>
+
+			<!-- user's bio -->
+			<div class="bio-container">
+				<p class="bio">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, 
+					doloribus sed hic ipsum consectetur asperiores commodi. Officia autem eligendi, 
+					beatae, a qui odit iste voluptas atque at alias voluptatem omnis?
+				</p>
+			</div>
+
+		</center>
+	</div>
+	
+	<!--<div class="profile_area border shadow-lg rounded-3 p-3">
 		<?php
 			echo '<pre>';
 			var_dump($_SESSION['user_info']);
 			echo '</pre>';
 		?>
-	</div>
+	</div> -->
 </body>
 </html>
