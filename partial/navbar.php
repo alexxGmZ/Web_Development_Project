9eab35a7b46f20ec8da613c9b675afddea7d05ba
earlylalo <91583055+earlylalo@users.noreply.github.com>
2022-12-06 02:03:20 -0800

    Added sample navbar

--- partial/navbar.php
+++ partial/navbar.php
@@ -1,31 +1,43 @@
 <!-- Navigation Bar -->
-<nav class="border-bottom navbar sticky-top bg-light">
+<div class="border-bottom sticky-top bg-light p-2">
 	<div class="container-fluid">
-		<!-- Site Page Name / Logo -->
-		<div class="col-auto d-flex justify-content-start ms-3">
-			<a class="teal_text navbar-brand fw-bolder fs-2" href="landing.php">
-				<img class="" src="./assets/images/navbar logo 1 trimmed.png">
-			</a>
-		</div>
-		<!-- End of Site Page Name / Logo -->
+		<nav class="navbar navbar-light bg-light d-lg-flex justify-content-lg-between">
+			
+			<!-- Site Page Name / Logo -->
+			<div class="ms-3 d-flex justify-content-md-center">
+				<a class="teal_text navbar-brand fw-bolder fs-2" href="landing.php">
+					<img class="" src="./assets/images/navbar logo 1 trimmed.png">
+				</a>
+			</div>
+			<!-- End of Site Page Name / Logo -->
 
-		<div class="col-auto d-inline-flex justify-content-center">
-			<!-- Search Bar and Icon Button -->
-			<form class="d-flex" action="https://google.com/search" target="_blank" type="GET">
-				<input class="form-control ms-2 me-2" type="search" placeholder="Search" aria-label="Search">
-				<button class="btn btn-outline-success btn-sm me-4" type="submit">
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-						<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-					</svg>
+			<div class="">
+				<!-- Search Bar and Icon Button -->
+				<form class="d-flex" action="https://google.com/search" target="_blank" type="GET">
+					<input class="form-control ms-2 me-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success btn-sm me-4" type="submit">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+							<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+						</svg>
+					</button>
+				</form>
+				<!-- End of Search Bar and Icon Button -->
+			</div>
+
+			
+				<button class="m-2 navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
 				</button>
-			</form>
-			<!-- End of Search Bar and Icon Button -->
-		</div>
+				<div class="collapse navbar-collapse" id="navigation">
+					<div class="navbar-nav mr-auto">
+						<div class="d-flex justify-content-center">
+							<a class="btn btn-outline-primary m-2 rounded-pill" href="login.php">Log-in</a>
+							<a class="btn btn-primary m-2 rounded-pill" href="registration.php">Sign-up</a>
+						</div>	
+					</div>
+				</div>
 
-		<div class="col-auto d-inline-flex justify-content-end">
-			<a class="btn btn-outline-primary me-2 rounded-pill" href="login.php">Log-in</a>
-			<a class="btn btn-primary rounded-pill me-3" href="registration.php">Sign-up</a>
-		</div>
+		</nav>
 	</div>
-</nav>
+</div>
 <!-- End of Navigation Bar -->
