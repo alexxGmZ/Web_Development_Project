7cc65eec880aef9d6d48fe26c463031900802d31
alexxShandsome <algilbertgomez@gmail.com>
2022-12-15 10:46:31 +0800

    shorten the code

--- landing.php
+++ landing.php
@@ -15,90 +15,19 @@
 
 <?php
 	// funniest meme layout design function
-	function top_meme_layout($post){
+	function posted_meme_layout($post, $index){
 		// debugging purposes
 		// var_dump($post);
 ?>
 		<div class="row rounded-2 border shadow mb-4 pt-3 pb-3 ps-2 pe-2">
-			<h1 class="text-center mb-4">Funniest Meme of the Day</h1>
+			<?php if ($index == 0): ?>
+				<h1 class="text-center mb-4">Funniest Meme of the Day</h1>
+			<?php endif ?>
 
 			<h3 class="text-center"><?php echo $post["TITLE"];?></h3>
 
 			<div class="text-center border rounded-2 p-2">
 				<img class="img-fluid" src="./uploaded_files/memes_posted/<?php echo $post['POST_IMAGE'];?>">
-			</div>
-
-			<div class="row pt-3">
-				<!-- when not logged in, redirect buttons to login page -->
-				<?php if (!isset($_SESSION['is_logged_in'])): ?>
-					<div class="col justify-content-end d-inline-flex">
-						<a class="btn btn-outline-primary" type="button" href="./login.php">
-							<div class="row g-0">
-								<img class="col me-1" src="./assets/icons/caret-up.svg">
-								<label class="col"><?php echo $post["UPVOTE"];?></label>
-							</div>
-						</a>
-					</div>
-					<div class="col-auto p-0 text-center">
-						<a class="btn btn-outline-danger" type="button" href="./login.php">
-							<div class="row g-0">
-								<img class="col me-1" src="./assets/icons/caret-down.svg">
-								<label class="col"><?php echo $post["UPVOTE"];?></label>
-							</div>
-						</a>
-					</div>
-					<div class="col">
-						<a class="btn btn-outline-success rounded-pill" type="button" href="./login.php">
-							<div class="row g-0">
-								<img class="col me-1" src="./assets/icons/chat-left.svg">
-								<label class="col">0</label>
-							</div>
-						</a>
-					</div>
-
-				<?php else: ?>
-					<div class="col justify-content-end d-inline-flex">
-						<button class="btn btn-outline-primary" type="button">
-							<div class="row g-0">
-								<img class="col me-1" src="./assets/icons/caret-up.svg">
-								<label class="col"><?php echo $post["UPVOTE"];?></label>
-							</div>
-						</button>
-					</div>
-					<div class="col-auto p-0 text-center">
-						<button class="btn btn-outline-danger" type="button">
-							<div class="row g-0">
-								<img class="col me-1" src="./assets/icons/caret-down.svg">
-								<label class="col"><?php echo $post["UPVOTE"];?></label>
-							</div>
-						</button>
-					</div>
-					<div class="col">
-						<button class="btn btn-outline-success rounded-pill" type="button">
-							<div class="row g-0">
-								<img class="col me-1" src="./assets/icons/chat-left.svg">
-								<label class="col">0</label>
-							</div>
-						</button>
-					</div>
-				<?php endif ?>
-			</div>
-		</div>
-<?php
-	}
-?>
-
-<?php
-	// ordynary meme layout design function
-	function ordinary_meme_layout($post){
-		// debugging purposes
-		// var_dump($post);
-?>
-		<div class="row rounded-2 border shadow-lg mb-4 pt-3 pb-3 ps-2 pe-2">
-			<h3 class="text-center"><?php echo $post["TITLE"];?></h3>
-
-			<div class="text-center border p-2">
-				<img class="img-fluid" src="./uploaded_files/memes_posted/<?php echo $post["POST_IMAGE"];?>">
 			</div>
 
 			<div class="row pt-3">
@@ -196,12 +125,7 @@
 				if($post[$index]['DOWNVOTE'] == NULL)
 					$post[$index]['DOWNVOTE'] = 0;
 
-				if ($index == 0){
-					top_meme_layout($post[$index]);
-				}
-				else{
-					ordinary_meme_layout($post[$index]);
-				}
+				posted_meme_layout($post[$index], $index);
 			}
 		?>
 	</div>
